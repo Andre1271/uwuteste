9495cc39f9a730811841889919caed347c1388f1
Andre1271 <andrebschevenin@gmail.com>
2024-11-07 20:35:43 -0300

    Update umarquivo.ps1

--- umarquivo.ps1
+++ umarquivo.ps1
@@ -1,10 +1,9 @@
-Add-Type -TypeDefinition @"
+powershell -Command "Add-Type -TypeDefinition @'
 using System;
 using System.Windows.Forms;
 public class MessageBoxClass {
     public static void Show(string message) {
-        MessageBox.Show(message, "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        MessageBox.Show(message, 'Alerta', MessageBoxButtons.OK, MessageBoxIcon.Warning);
     }
 }
-"@
-[MessageBoxClass]::Show("Olá, eu fui executado")
+'@; [MessageBoxClass]::Show('Olá, eu fui executado')"
