fec974a708209fac1efec0470245a6a0954c9067
Andre1271 <andrebschevenin@gmail.com>
2024-11-07 20:33:21 -0300

    Create umarquivo.ps1

---
+++ umarquivo.ps1
@@ -0,0 +1,10 @@
+Add-Type -TypeDefinition @"
+using System;
+using System.Windows.Forms;
+public class MessageBoxClass {
+    public static void Show(string message) {
+        MessageBox.Show(message, "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+    }
+}
+"@
+[MessageBoxClass]::Show("Olá, eu fui executado")
