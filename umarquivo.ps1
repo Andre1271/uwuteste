12a177675a4792e3f7e37523dedc478b13cac561
Andre1271 <andrebschevenin@gmail.com>
2024-11-07 21:09:15 -0300

    Update umarquivo.ps1

--- umarquivo.ps1
+++ umarquivo.ps1
@@ -1,10 +1,12 @@
-Add-Type -TypeDefinition @"
-using System;
-using System.Windows.Forms;
-public class MessageBoxClass {
-    public static void Show(string message) {
-        MessageBox.Show(message, 'Alerta', MessageBoxButtons.OK, MessageBoxIcon.Information);
+function Show-WarningPopup {
+    param (
+        [string]$message,
+        [string]$title
+    )
+    $popupJob = Start-Job -ScriptBlock {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show($using:message, $using:title, [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
     }
 }
-"@
-[MessageBoxClass]::Show('Olá')
+
+Show-WarningPopup -message "You have been warned!" -title "Title"
