082c4ac77d3ad4272fcdee438accda127c9e8a5c
Andre1271 <andrebschevenin@gmail.com>
2024-11-25 18:31:51 -0300

    Update umarquivo.ps1

--- umarquivo.ps1
+++ umarquivo.ps1
@@ -1,31 +1,22 @@
-param (
-    [string]$ShortcutPath
-)
-
 try {
-    # Verificar se o caminho do atalho foi passado
-    if (-not $ShortcutPath) {
-        Write-Host "Erro: O caminho do atalho não foi passado!" -ForegroundColor Red
-        Exit 1
-    }
-
-    # Validar se o arquivo de atalho existe
-    if (-not (Test-Path $ShortcutPath)) {
-        Write-Host "Erro: O caminho fornecido não é válido ou o atalho não existe!" -ForegroundColor Red
-        Exit 1
-    }
-
-    # Obter o diretório e nome do atalho
-    $shortcutDir = [System.IO.Path]::GetDirectoryName($ShortcutPath)
-    $shortcutName = [System.IO.Path]::GetFileNameWithoutExtension($ShortcutPath)
-
-    Write-Host "Diretório do atalho: $shortcutDir"
-    Write-Host "Nome do atalho: $shortcutName"
-
-    # Criar o caminho para o PDF
-    $outputPDFPath = Join-Path -Path $shortcutDir -ChildPath "$shortcutName.pdf"
-    Write-Host "PDF será salvo como: $outputPDFPath"
-
+    # Captura o caminho do script PowerShell executado
+    $scriptPath = $MyInvocation.MyCommand.Path
+    
+    # Pega o diretório onde o script está sendo executado
+    $scriptDir = [System.IO.Path]::GetDirectoryName($scriptPath)
+    
+    # Obter o nome do script
+    $scriptName = [System.IO.Path]::GetFileNameWithoutExtension($scriptPath)
+    
+    # Mostrar no console
+    Write-Host "Diretório do script: $scriptDir"
+    Write-Host "Nome do script: $scriptName"
+    
+    # Criar um caminho para salvar um arquivo PDF no mesmo diretório
+    $outputPDFPath = Join-Path -Path $scriptDir -ChildPath "$scriptName.pdf"
+    Write-Host "Arquivo PDF será salvo como: $outputPDFPath"
+    Write-Host "Pressione qualquer tecla para sair..." -ForegroundColor Cyan
+    Read-Host
 } catch {
     Write-Host "Erro: $_" -ForegroundColor Red
     $_ | Out-File -FilePath "$env:TEMP\script_error.log" -Append
